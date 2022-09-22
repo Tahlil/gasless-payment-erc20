6fca44fc5ffab780db801a856372bd6287678a43
Tahlil <bsse0803@iit.du.ac.bd>
2022-09-22 17:28:33 +0600

    Premint 10000000 tokens

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -6,7 +6,9 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 
 contract UMToken is ERC20, Ownable, ERC20Permit {
-    constructor() ERC20("UMToken", "um") ERC20Permit("UMToken") {}
+    constructor() ERC20("UMToken", "um") ERC20Permit("UMToken") {
+        mint(msg.sender, 10000000 * 10 ** decimals());
+    }
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
